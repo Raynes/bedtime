d85cfaf74a0269dd6455ae40cb40d968621c215d
Anthony Grimes <i@raynes.me>
2013-03-03 02:11:50 -0800

    Can now stop timers. Countdown will be added next.

--- src/bedtime/core.clj
+++ src/bedtime/core.clj
@@ -5,7 +5,7 @@
             [clj-time.core :as t]
             [clj-time.format :as f]
             [me.raynes.conch :refer [programs]]
-            [overtone.at-at :refer [mk-pool after]]))
+            [overtone.at-at :refer [mk-pool after stop]]))
 
 (s/native!)
 
@@ -21,44 +21,63 @@
          (s/alert "Time must be of hour:minute:second format!"))))
 
 (defn calculate-time [time]
-  (when-let [[hour minute second] ((juxt t/hour t/minute t/secs)
-                                   (validate-input time))]
-    (let [now (t/now)]
-      (t/in-msecs
-       (t/interval now
-                   (t/plus now
-                           (t/hours hour)
-                           (t/minutes minute)
-                           (t/secs second)))))))
+  (when-let [parsed (validate-input time)]
+    (let [now (t/now)
+          [hour minute second] ((juxt t/hour t/minute t/sec) parsed)
+          then (t/plus now (t/hours hour) (t/minutes minute) (t/secs second))
+          interval (t/interval now then)]
+      {:now now
+       :then then
+       :ms (t/in-msecs interval)
+       :interval interval})))
 
-(defn sleep-event [t]
+(declare new-timer)
+
+(defn countdown [frame timer-map]
+  (let [{:keys [then interval]} timer-map
+        panel (sm/mig-panel
+               :items [[(s/label "foo") "align center, wrap"]
+                       [(s/button :text "Stop"
+                                  :listen [:action (fn [_]
+                                                     (stop (:job @timer))
+                                                     (doto frame
+                                                       (.setContentPane (new-timer frame))
+                                                       (s/pack!)))])]])]
+    panel))
+
+(defn sleep-event [frame t]
   (fn [e]
-    (let [time (s/text t)]
+    (when-let [{:keys [ms] :as timer-map} (calculate-time (s/text t))]
       (swap! timer assoc
-             :job (after (calculate-time time)
+             :job (after ms
                          #(osascript "-e" "tell application \"System Events\" to sleep")
-                         pool)))))
+                         pool))
+      (doto frame
+        (.setContentPane (countdown frame timer-map))
+        (s/pack!)))))
 
+(frame)
 (defn time-field []
   (let [t (s/text :text "1:0:0"
                   :editable? true)]
     (doto t
       (when-focused-select-all))))
 
-(defn layout []
+(defn new-timer [frame]
   (let [t (time-field)]
     (sm/mig-panel
      :items [[(s/label "Time: ") "grow"]
              [t "wmin 100, wrap"]
              [(s/button :text "Night!"
-                        :listen [:action (sleep-event t)])
+                        :listen [:action (sleep-event frame t)])
               "grow, span"]])))
 
 (defn frame []
-  (doto (s/frame
-         :title "Bedtime"
-         :on-close :dispose
-         :content (layout)
-         :resizable? true)
-    (s/pack!)
-    (s/show!)))
+  (let [f (s/frame
+           :title "Bedtime"
+           :on-close :dispose
+           :resizable? true)]
+    (doto f
+      (.setContentPane (new-timer f))
+      (s/pack!)
+      (s/show!))))
