10f6e00dd43e6e07a522f8216bc4cf5dc0d8c941
Anthony Grimes <i@raynes.me>
2013-03-03 02:11:30 -0800

    Update seesaw and clojure.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/Raynes/bedtime"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [seesaw "1.4.2"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [seesaw "1.4.3"]
                  [com.miglayout/miglayout "3.7.4"]
                  [clj-time "0.4.4"]
                  [me.raynes/conch "0.5.1"]
